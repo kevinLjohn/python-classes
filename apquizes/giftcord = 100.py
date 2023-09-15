e8cbc17d25cb75117de46f23b9e2b7ebc0abf1f5
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2023-09-15 14:34:02 +0000

    test is finished ya bot

---
+++ apquizes/giftcord = 100.py
@@ -0,0 +1,5 @@
+giftcord = 100.00
+addedcash = 50.00
+friend = 35.50
+total = (str(friend + giftcord + addedcash))
+print("You have $" + total + " cash in your account")
--- apquizes/quiz1.py
+++ apquizes/quiz1.py
@@ -1,9 +1,17 @@
 # 1. In your on words, describe what a variable is.
 
+#"A variable is a value that changes depending on what you put after or before it"
+
 # 2. Name three (3) operators we've learned about in class and describe 
 # a situation where you would use each one. 
+#>=
+#=
+#+=
 
+"a situation where you'd use this is when you need to calculate something"
 # 3. Describe the difference between camal case and pascal case.
+
+#Camal case is "numberOfPeople" while pascal case is "NumberOfDeadpeople"
 
 # 4. Which operator is being used in the example below
 numberOfApples = 10
@@ -11,13 +19,25 @@ numberOfOranges = 12
 
 numberOfApples < numberOfOranges
 
+#"lass than equal to"
+
 # 5. You have been given a $100.00 dollar gift card from a family member. Another 
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
 
+giftcord = 100.00
+addedcash = 50.00
+friend = 35.50
+total = (str(friend + giftcord + addedcash))
+print("You have $" + total + " cash in your account")
+
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+
+name = input("what is your name? ")
+
+print("hi " + name)
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
@@ -25,12 +45,23 @@ numberOfApples < numberOfOranges
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
 
+name = input("what is your name? ")
+age = input("how old are you? ")
+allergy = input("do you have alargies and if yes what are they? ")
+foood = input("what is your favorite food? ")
+
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.
 # If the user DOES have a glutten allergy, they need to eat a special type of bread, if the user DOES NOT
 # have an allergy they can eat the regular bread. What data type would best be used in this 
 # scenario and how would you write a variable to represent this value?
 
+#The best varible to use here would be a if command and input
+
 # 9. In your own words, describe what a program is?
 
+#A program is a list of code that makes a device/website work
+
 # 10. Describe the difference between a float and an integer. 
+
+#a float is a decimal while a interger is a full number
