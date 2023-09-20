3f69545bc1a614572467b9f7caab4d8fa3d11960
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2023-09-20 15:23:09 +0000

    .

---
+++ apquizes/classwork9/20.py
@@ -0,0 +1,21 @@
+# convert the following datatypes into floats.
+a = 12
+b= 100
+print(float(a))
+print(float(b))
+# convert the following floats into integers.
+c= 12.34
+d= 23.33
+print(int(c))
+print(int(d))
+# modify the code below to print an integer number. 
+num_one= 20
+num_two= 23.45
+
+print(int(num_one + num_two)
+)
+# modify the following code below to 
+name_of_pet = "Spuny"
+age_of_pet = 10
+
+print(str("this is my dog "+ name_of_pet + ". He is " + age_of_pet+ " years old."))
