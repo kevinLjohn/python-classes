7f80f6104efa9e1a158f5e3f14ea30f9657737a1
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2023-09-27 15:23:13 +0000

    classwork

---
+++ apquizes/apactivities/conditionalActivity.py
@@ -0,0 +1,37 @@
+# 1. What is the difference between 
+# a parameter and an argument in a python function
+"The parameter is a placeholder and the argument is the code used"
+# 2. In your own words, describe what 
+# a conditional statement (if/else) is 
+"A if/else statement is a function"
+# 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
+goop = input("input an number ")
+ if (goop = 10):
+ print("The nuber you choose is equip to the true number")
+else
+   (print("Your number is less than or greater than the true number")
+     
+# 3. Write a conditional statement for food in the refridgerator.
+# your conditional statement should be wrapped in a function that takes one (1)
+# parameter. The data type for this parameter should be true or false. 
+# your function should have a line of code that asks the user if there is food in the refridgerator.
+# If there is food in the refridgerator, print time to cook. If there is 
+# NOT food in the fridge, print time to shop. 
+# When you call your function there should be an argument that accepts 
+# a boolean.
+
+Food = True
+
+
+
+if food = "yes":
+   print("time to cook") 
+
+else food == False
+   print("time to shop")   
+
+# 4. Create a function that checks the inventory of cereal for a store. 
+# your function should parameter should accept an integer. In your function
+# use a conditional statement to determine if you need to order more cereal.
+# If there is more than 10 boxes, print "inventory full", else if there are less than 
+# 10 boxes print "we need to order more cereal".
