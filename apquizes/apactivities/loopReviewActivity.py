203a1238d2d4cc7116193cd2cc913186b2b24867
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2023-10-19 14:02:30 +0000

    completed work

---
+++ apquizes/apactivities/loopReviewActivity.py
@@ -0,0 +1,51 @@
+# Create while loops for the following conditions
+
+
+# 1. Create a security camera program that uses a while loop to detect if there is an
+# object in site. 
+camera4b = input("Is there anything on camera 4B? ")
+
+while camera4b = 'yes'
+    print("object spotted")
+    cam4b = input("Is it still there? ")
+    break
+
+while cam4b = 'yes'
+    print("object spotted")
+    print(cam4b)
+    break
+
+while camera4b = 'no'
+    print('good')
+    break
+
+while cam4b = 'no'
+    print('good')
+    break
+# 2. Create a Printer Loop program that will continue to print copies of a document based on the number
+# that the user inputs. 
+count= int(input('how many cpoys do you want? '))
+
+while count >= 0
+    print('copy made')
+    count += 1
+    break
+# 3. Create a Stop Light Loop that will change the light color based on different time intervals. 
+# every 30 seconds the light should change between green and red. 
+
+light = 1
+
+
+while light < 30
+    print('green light')
+    light += 1
+    break
+
+while light > 60
+    print('red light')
+    light -= 1
+    break
+
+
+# 3. BONUS: add an additional condition that will change the light to yellow for 5 seconds before the
+# next light change. 
---
+++ apquizes/apactivities/whileloops.py
@@ -0,0 +1,15 @@
+washer = int(input('How many minutes do you wanna put your cloths in the dryer for? '))
+
+
+while washer < 60
+    print("Your clothes are still wet")
+    washer2 = input('How many minutes will you like to add')
+    break
+
+while washer < 59
+    print('Your clothes are dry now')
+    break
+
+while washer2 < 59
+    print('Your clothes are dry now')
+    break
