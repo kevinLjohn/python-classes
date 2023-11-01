1b1b0a525d612b33231fd6626177cc9f2c9d2832
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2023-11-01 03:46:37 +0000

    .

---
+++ apquizes/apactivities/Cesiumwritingassignment.py
@@ -0,0 +1,10 @@
+
+
+# My experience at the Cesium building was great. The people there were nice.
+#They gave us free snacks. They made it interactive and well oragnized.
+
+# Something I learned from there is that unreal editor is used for 3d games. Unity is used for 2D gaames. They used
+#UE5 to make their progarm. If you give them the right infomation their p[rogarm and simulate underwater or places not yet added.
+
+# Something else I learned was that they used google to get most of their locations. 
+# They used google for the places they didn't have done.They also made break time look amazing there.
---
+++ apquizes/apactivities/mondaywork10/30.py
@@ -0,0 +1,29 @@
+MONDAY ASYNC ACTIVITY
+
+Please visit and read the articles found on the links below:
+
+- Documentation
+https://www.writethedocs.org/guide/writing/beginners-guide-to-docs/#:~:text=Your%20first%20steps%20in%20documentation,will%20serve%20your%20project%20well.
+
+- Git Commit Messages
+https://dev.to/chrissiemhrk/git-commit-message-5e21
+
+When you are finished reading the articles, answer the following questions and commit and push 
+your work to your repository. 
+
+Your document should be named as followed async_Work_oct30.txt.
+
+This assignment is due by the end of class in order to get credit. 
+_______________________________________________________________________________
+
+1. List and explain three 3 reasons why programmers write documentation. 
+
+#They use documents to keep track of their work. They use it to communticate their ideas. they use it to leave ideas for later when they want to remember it. 
+
+2. In your own words, describe what a README.md document is.
+
+3. Based on what your read, what are four (4) things that should be in a README document. 
+
+4. Name and describe three (3) reasons why programmers write commmit messages when using GitHub.
+
+5. Describe three things (3) negative outcomes that could happen if a programmer has bad or no documentation.
