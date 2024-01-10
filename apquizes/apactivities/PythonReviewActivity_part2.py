28dd8ecd7db4f06afea130ebdfe22bd9c39ad578
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2024-01-10 16:23:24 +0000

    mad lib

---
+++ apquizes/apactivities/PythonReviewActivity_part2.py
@@ -0,0 +1,21 @@
+#  Winter break Python Mad libs 
+# Create a short mad lib story about your winter break. 
+# Your mad lib should include 6 input fields for users to enter data.
+# Your mad lib program should prompt the user to enter 2 nouns, 2 verbs, and 2 adjectives. 
+# Your mad lib should be written in complete sentences. 
+# Once completed, submit your madlib to your github repository by using the source control tool in codespaces. 
+
+adjective_one = input("input a adjective")
+noun_one = input("input a noun ") 
+verb_on = input("input a verb ")
+adjective_two = input("input a second adjective")
+noun_two = input("input a second noun ") 
+verb_two = input("input a second verb ")
+adjective_three = input("input a third adjective")
+noun_three = input("input a third noun ") 
+verb_three = input("input a third verb ")
+
+print("A adventure")
+print("A " + adjective_one + " " + noun_one + " went on a adventure away from " + noun_two ".")
+print("He starts his adventure by " + verb_on + " . He then later on encounters a " + adjective_two + " " + noun_three + " . he fights hard and finally defeats it")
+print(" He then start to head home, after a long and " + adjective_three + " adventure. He finally gets to go back to " + verb_two + ". and oh boy does he like " + verb_three) 
