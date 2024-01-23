48bce20740313f6cca34b629fefa3228f5ca32ef
kevinLjohn <108888414+kevinLjohn@users.noreply.github.com>
2024-01-23 16:25:52 +0000

    makeup work

---
+++ apquizes/apactivities/privicyassigment.py
@@ -0,0 +1,8 @@
+#Using complete sentences, write three (3) things you learned from the documentary.
+I learned how to escape hackers
+i learned how to avoid hackers
+i learned to change my password daily
+#Google search your name, if you've found yourself on google, please describe what you found. Are surprised by what you found? using complete sentences.
+i didnt find myself at all
+#Based on what you've learned, what steps would you use to disapear online. Please use complete sentences.#
+I would change states
